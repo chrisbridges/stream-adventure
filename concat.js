846685664cf2dc51d733336e4333fcb47b1fc900
chris.bridges <christopher_bridges@icloud.com>
2018-08-10 16:55:19 -0400

    official solution included

--- concat.js
+++ concat.js
@@ -6,3 +6,14 @@ process.stdin.pipe(concat(body => {
   const reversedBody = body.toString().split('').reverse().join('')
   console.log(reversedBody)
 }))
+
+// Official solution
+
+// Here's the reference solution:
+
+// var concat = require('concat-stream');
+  
+// process.stdin.pipe(concat(function (src) {
+//     var s = src.toString().split('').reverse().join('');
+//     console.log(s);
+// }))
